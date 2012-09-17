d8ac9d11c48c57819ed99ba838e2b2aee0002251
Benedito Carneiro <ctsbc@yahoo.com.br>
2012-09-17 22:42:17 +0200

    Added Brazilian Portuguese translation

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * English language file for saveandedit plugin
+ *
+ * @author Benedito Carneiro <ctsbc@yahoo.com.br>
+ */
+
+// custom language strings for the plugin
+$lang['btn_saveandedit'] = 'Continuar editando, depois de salvar';
+
+
+
+//Setup VIM: ex: et ts=4 :
