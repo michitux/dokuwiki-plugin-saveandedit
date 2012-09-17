691c5ea4feaf5f226d0fa60a906db56270db9737
Michael Hamann <michael@content-space.de>
2012-09-17 22:44:20 +0200

    Added German translation

---
+++ lang/de/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * German language file for saveandedit plugin
+ *
+ * @author Michael Hamann <michael@content-space.de>
+ */
+
+// custom language strings for the plugin
+$lang['btn_saveandedit'] = 'Weiter bearbeiten nach dem Speichern';
+
+
+
+//Setup VIM: ex: et ts=4 :
