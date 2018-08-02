59296adfd5518f6f3ef3ed9424797a45dea2d029
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-08-03 03:12:29 +1000

    Create lang.php

---
+++ lang/fi/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Finnish language file for saveandedit plugin
+ *
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
+ */
+
+$lang['btn_saveandedit'] = 'Edelleen muokkaus tallennuksen jälkeen';
