c42a4edc323e0e57843f53ebfaddea4ca306bfe5
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-08-03 03:12:37 +1000

    Create lang.php

---
+++ lang/pt/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Portuguese language file for saveandedit plugin
+ *
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
+ */
+
+$lang['btn_saveandedit'] = 'Continue após a edição de verão';
