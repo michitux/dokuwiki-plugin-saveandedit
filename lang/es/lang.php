fa063888d82657cb2ad729877b9f0753ee59321a
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-08-03 03:12:26 +1000

    Create lang.php

---
+++ lang/es/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Spanish language file for saveandedit plugin
+ *
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
+ */
+
+$lang['btn_saveandedit'] = 'Continuar editando después de guardar';
