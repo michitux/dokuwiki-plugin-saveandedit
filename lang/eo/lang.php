ed8ccffacbf56e292860af1e7fcb55f5630ea686
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-08-03 03:12:24 +1000

    Create lang.php

---
+++ lang/eo/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Esperanto language file for saveandedit plugin
+ *
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
+ */
+
+$lang['btn_saveandedit'] = 'Daŭrigu redaktadon post konservado';
