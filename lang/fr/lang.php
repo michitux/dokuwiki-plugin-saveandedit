47199fc371d7421e0c5a2b0b6120cf9213d95724
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-08-03 03:12:50 +1000

    Update lang.php

---
+++ lang/fr/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * French language file for saveandedit plugin
+ *
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
+ */
+
+$lang['btn_saveandedit'] = 'Continuer à éditer après avoir enregistré';
