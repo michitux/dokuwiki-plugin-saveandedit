be88ec59690f6d8a5c1e948b35a5866889930ff9
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2018-08-03 03:12:38 +1000

    Create lang.php

---
+++ lang/ru/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Russian language file for saveandedit plugin
+ *
+ * @author Rainbow-Spike <rainbow_spike@derpy.ru>
+ */
+
+$lang['btn_saveandedit'] = 'Продолжить редактирование после сохранения';
