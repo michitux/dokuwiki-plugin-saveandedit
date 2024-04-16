1c4557b2dfbe709733824734db7e189d01e03dbb
Gregory Loichot <gregory.loichot@unige.ch>
2024-04-16 16:38:10 +0200

    Add french translation

---
+++ lang/fr/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * French language file for saveandedit plugin
+ *
+ * @author Michael Hamann <michael@content-space.de>
+ */
+
+// custom language strings for the plugin
+$lang['btn_saveandedit'] = 'Continuer à éditer après enregistrement';
+
+
+
+//Setup VIM: ex: et ts=4 :
